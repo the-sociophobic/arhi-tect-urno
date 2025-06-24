72b9601527090a10402a77f8c306c1111e70ebfc
the-sociophobic <torikowashi@gmail.com>
2025-06-24 04:18:33 +0300

    add video /test3

---
+++ site-next/app/test3/Sphere.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import MagicSphere from '../lib/components/Three/MagicSphere'
+import ThreeScene from '../lib/components/Three/ThreeScene'
+import generatePath from '../lib/utils/generatePath'
+
+
+const Sphere = () => {
+  return (
+    <>
+      <video
+        className='d-none'
+        id='video'
+        loop
+        muted
+        crossOrigin='anonymous'
+      // playsinline
+      >
+        {/* <source src='./arhi-tect-urno/hdri/test.mov' /> */}
+        <source src={generatePath('/three/video/3.mp4')} />
+      </video>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col mb-5'>
+            <div className='Architects__Three-container'>
+              <ThreeScene
+                orthographic={false}
+                className='Architects__Three'
+              >
+                <MagicSphere />
+              </ThreeScene>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+export default Sphere
