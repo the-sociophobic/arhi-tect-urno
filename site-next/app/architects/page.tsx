a519d4dc347865ed6b8ca6a646bb3ec1f327ec3e
the-sociophobic <torikowashi@gmail.com>
2025-07-10 02:45:47 +0300

    upd Math.random()

--- site-next/app/architects/page.tsx
+++ site-next/app/architects/page.tsx
@@ -1,7 +1,10 @@
+import { connection } from 'next/server'
+
 import Sphere from './Sphere'
 
 
-const Page = () => {
+const Page = async () => {
+  await connection()
   const randomVideoURL = `/three/video/${1 + Math.round(Math.random() * 2)}.mp4`
 
   return <Sphere randomVideoURL={randomVideoURL} />
