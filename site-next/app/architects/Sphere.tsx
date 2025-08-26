cd38256ca6a4611a82e33b61d8b536eaaa7fe22f
the-sociophobic <torikowashi@gmail.com>
2025-08-26 22:33:59 +0300

    add video play error handling

--- site-next/app/architects/Sphere.tsx
+++ site-next/app/architects/Sphere.tsx
@@ -1,9 +1,13 @@
 'use client'
 
 import { FC, useEffect, useState } from 'react'
+import Image from 'next/image'
+
 import MagicSphere from '../lib/components/Three/MagicSphere'
 import ThreeScene from '../lib/components/Three/ThreeScene'
 import generatePath from '../lib/utils/generatePath'
+
+import playSvgSrc from '../lib/assets/images/play.svg'
 
 
 const Sphere: FC<{
@@ -13,44 +17,75 @@ const Sphere: FC<{
   randomVideoURL,
   customVideoURL
 }) => {
-  useEffect(() => {
-    const video = document.getElementById('video');
-    (video as HTMLVideoElement)?.play?.()
-  }, [])
+    const [showPlay, setShowPlay] = useState(false)
 
-  const [videoLoaded, setVideoLoaded] = useState(false)
+    useEffect(() => {
+      const video = document.getElementById('video');
+      try {
+        (video as HTMLVideoElement)?.play?.()
+        setShowPlay(true)
+      } catch (err) {
+        console.log('video error')
+        console.log(err)
+        setShowPlay(true)
+      }
+    }, [])
 
-  return (
-    <>
-      <video
-        className='d-none'
-        id='video'
-        loop
-        muted
-        playsInline
-        crossOrigin='anonymous'
-        onPlay={e => setVideoLoaded(true)}
-      >
-        <source
-          src={customVideoURL || generatePath(randomVideoURL)}
+    const [videoLoaded, setVideoLoaded] = useState(false)
+
+    return (
+      <>
+        <video
+          className='d-none'
+          id='video'
+          loop
+          muted
+          playsInline
+          crossOrigin='anonymous'
+          onPlay={e => setVideoLoaded(true)}
+        >
+          <source
+            src={customVideoURL || generatePath(randomVideoURL)}
           // resource=''
-        />
-      </video>
+          />
+        </video>
 
-      <div className='row'>
-        <div className='col mb-5'>
-          <div className='Architects__Three-container'>
-            {videoLoaded &&
-              <ThreeScene className='Architects__Three'>
-                <MagicSphere />
-              </ThreeScene>
-            }
+        <div className='row'>
+          <div className='col mb-5'>
+            <div className='Architects__Three-container'>
+              {videoLoaded &&
+                <ThreeScene className='Architects__Three'>
+                  <MagicSphere />
+                </ThreeScene>
+              }
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  )
-}
+        {showPlay &&
+          <div
+            className='position-fixed'
+            onClick={() => {
+              const video = document.getElementById('video');
+              (video as HTMLVideoElement)?.play?.()
+              setShowPlay(false)
+            }}
+            style={{
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              zIndex: 1000,
+              cursor: 'pointer'
+            }}
+          >
+            <Image
+              src={playSvgSrc}
+              alt='logo'
+            />
+          </div>
+        }
+      </>
+    )
+  }
 
 
 export default Sphere
